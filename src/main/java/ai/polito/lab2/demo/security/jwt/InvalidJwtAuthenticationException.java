50036b03cb3acd3d9cdfb569fbff6e39ffdaa8d9
david <s255359@studenti.polito.it>
2019-10-22 10:36:07 +0200

    modifica v2 modificati gli end user

--- src/main/java/ai/polito/lab2/demo/security/jwt/InvalidJwtAuthenticationException.java
+++ src/main/java/ai/polito/lab2/demo/security/jwt/InvalidJwtAuthenticationException.java
@@ -3,13 +3,13 @@ package ai.polito.lab2.demo.security.jwt;
 import org.springframework.security.core.AuthenticationException;
 
 import javax.servlet.http.HttpServletResponse;
-import java.net.http.HttpResponse;
+
 
 public class InvalidJwtAuthenticationException extends AuthenticationException {
     public InvalidJwtAuthenticationException(String e) {
 
         super(e);
-        HttpServletResponse httpServletResponse = null;
-        httpServletResponse.setStatus(401);
+       /* HttpServletResponse httpServletResponse = null;
+        httpServletResponse.setStatus(401);*/
     }
 }
