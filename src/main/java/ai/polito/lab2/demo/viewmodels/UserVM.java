bb4ab4cb06e08ad27b8d1541edc25ecb4466cf96
Mauro Bellinazzi <mauro.bellinazzi@ovalmoney.com>
2020-08-11 12:02:58 +0200

    add isEnabled flag and renamed disenabled in disabled

--- src/main/java/ai/polito/lab2/demo/viewmodels/UserVM.java
+++ src/main/java/ai/polito/lab2/demo/viewmodels/UserVM.java
@@ -1,8 +1,11 @@
 package ai.polito.lab2.demo.viewmodels;
 
+import ai.polito.lab2.demo.Entity.Role;
 import lombok.Builder;
 import lombok.Data;
 import org.bson.types.ObjectId;
+
+import java.util.List;
 
 @Builder
 @Data
@@ -10,4 +13,6 @@ public class UserVM {
     private String username;
     private String family_name;
     private String userID;
+    private boolean isEnabled;
+    //private List<Role> roles;
 }
