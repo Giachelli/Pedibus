98f2e83736be78aa733fa51518d82288bde8cf14
Giachelli <giacomo.chelli4@gmail.com>
2020-09-19 10:58:11 +0200

    Merge branch 'master' of /Users/Giacomo/Desktop/Pedibus/Pedibus with conflicts.

--- src/main/java/ai/polito/lab2/demo/viewmodels/ChildAllVM.java
+++ src/main/java/ai/polito/lab2/demo/viewmodels/ChildAllVM.java
@@ -3,12 +3,15 @@ package ai.polito.lab2.demo.viewmodels;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+
 @Data
 @Builder
 public class ChildAllVM {
     private String childID;
     private String username;
     private String family_name;
+    @Min(3)
     private String nameChild;
     private boolean isMale;
 }
